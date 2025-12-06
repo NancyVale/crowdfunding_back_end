42dc0d54650a746fcfbd78a9c7a3f2c5e3ea9d1f
unknown <druchas99@hotmail.com>
2025-12-06 11:20:14 +1000

    pledges-second-part

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -2,9 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
 from .models import Fundraiser, Pledge
-from .serializers import FundraiserSerializer, PledgeSerializer, FundraiserDetailSerializer
+from .serializers import FundraiserSerializer, PledgeSerializer, FundraiserDetailSerializer, PledgeDetailSerializer
 from rest_framework import status, permissions
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly,IsSupporterOrReadOnly
 class FundraiserList(APIView):
   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
   def get(self, request):
@@ -74,6 +74,41 @@ class PledgeList(APIView):
                serializer.data,
                status=status.HTTP_201_CREATED
            )
+       return Response(
+           serializer.errors,
+           status=status.HTTP_400_BAD_REQUEST
+       )   
+   
+class PledgeDetail(APIView):
+   permission_classes = [
+      permissions.IsAuthenticatedOrReadOnly,
+      IsSupporterOrReadOnly
+   ]
+
+   def get_object(self, pk):
+       try:
+           pledge = Pledge.objects.get(pk=pk)
+           self.check_object_permissions(self.request, pledge)
+           return pledge
+       except Pledge.DoesNotExist:
+           raise Http404
+
+   def get(self, request, pk):
+       pledge = self.get_object(pk)
+       serializer = PledgeDetailSerializer(pledge)
+       return Response(serializer.data)  
+   
+   def put(self, request, pk):
+       pledge = self.get_object(pk)
+       serializer = PledgeDetailSerializer(
+           instance=pledge,
+           data=request.data,
+           partial=True
+       )
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data)
+
        return Response(
            serializer.errors,
            status=status.HTTP_400_BAD_REQUEST
